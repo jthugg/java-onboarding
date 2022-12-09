41025181619e587facc3090bcd458005804dcf4a
jthugg <jthug@naver.com>
2022-12-10 06:13:15 +0900

    Feat: 예외 상황 조사 시퀀스 구성

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,8 +3,18 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
-    public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
-        return answer;
-    }
+
+	public static int solution(List<Integer> pobi, List<Integer> crong) {
+		if (validatePobiAndCrong(pobi, crong)) {
+			return -1;
+		}
+		return runPobiVsCrong(pobi, crong);
+	}
+
+	private static boolean validatePobiAndCrong(List<Integer> pobi, List<Integer> crong) {
+		if (validateLength(pobi, crong) || validateCoverPage(pobi, crong) || validatePage(pobi, crong)) {
+			return true;
+		}
+		return false;
+	}
 }
