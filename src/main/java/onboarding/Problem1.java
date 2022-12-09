f76ec8b38113929efe9eea9722162f7bbf0a5e0a
jthugg <jthug@naver.com>
2022-12-10 06:18:02 +0900

    Feat: 각 리스트에 시작면(0), 마지막면(401)이 있는지 검사 기능 추가

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -12,7 +12,7 @@ class Problem1 {
 	}
 
 	private static boolean validatePobiAndCrong(List<Integer> pobi, List<Integer> crong) {
-		if (validateLength(pobi, crong) || validateCoverPage(pobi, crong) || validatePage(pobi, crong)) {
+		if (validateLength(pobi, crong) || validateHasCoverPage(pobi, crong) || validatePage(pobi, crong)) {
 			return true;
 		}
 		return false;
@@ -20,6 +20,13 @@ class Problem1 {
 
 	private static boolean validateLength(List<Integer> pobi, List<Integer> crong) {
 		if (pobi.size() != 2 || crong.size() != 2) {
+			return true;
+		}
+		return false;
+	}
+
+	private static boolean validateHasCoverPage(List<Integer> pobi, List<Integer> crong) {
+		if (pobi.get(0) < 1 || pobi.get(1) > 400 || crong.get(0) < 1 || crong.get(1) > 400) {
 			return true;
 		}
 		return false;
