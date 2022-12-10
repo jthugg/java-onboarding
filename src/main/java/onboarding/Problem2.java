ef018767a147504216f94d7e1fdbd4f559c856b0
jthugg <jthug@naver.com>
2022-12-10 21:00:54 +0900

    Feat: 연속된 문자열을 지우는 기능 추가, Problem2 기능 구현 완료

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -13,6 +13,9 @@ public class Problem2 {
 	}
 
 	private static boolean hasDuplicatedPart(String cryptogram) {
+		if(cryptogram.length() == 0) {
+			return false;
+		}
 		return getDuplicatedPart(cryptogram).size() > 0;
 	}
 
@@ -23,11 +26,20 @@ public class Problem2 {
 			if (cryptogram.charAt(count - 1) == cryptogram.charAt(count)) {
 				duplicatedPart += cryptogram.charAt(count);
 			}
-			if (cryptogram.charAt(count - 1) != cryptogram.charAt(count)) {
-				duplicatedParts.add(duplicatedPart);
+			if ((cryptogram.charAt(count - 1) != cryptogram.charAt(count)) || count == cryptogram.length() - 1) {
+				if(duplicatedPart.length() > 1) {
+					duplicatedParts.add(duplicatedPart);
+				}
 				duplicatedPart = String.valueOf(cryptogram.charAt(count));
 			}
 		}
 		return duplicatedParts;
+	}
+
+	private static String removeDuplicatedPart(String cryptogram, List<String> duplicatedParts) {
+		for (String duplicatedPart : duplicatedParts) {
+			cryptogram = cryptogram.replace(duplicatedPart, " ");
+		}
+		return cryptogram.replaceAll(" ", "");
 	}
 }
