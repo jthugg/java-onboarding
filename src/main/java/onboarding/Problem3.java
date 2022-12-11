6b7e0c7869d8ad0187a5c8d2701edf5021a607e9
jthugg <jthug@naver.com>
2022-12-12 03:04:59 +0900

    Feat: 1부터 매개변수 number 까지 순회하며 3, 6, 9 중 어느 하나라도 포함하는지 검사 기능 추가

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,15 @@
 package onboarding;
 
 public class Problem3 {
-    public static int solution(int number) {
-        int answer = 0;
-        return answer;
-    }
+
+	public static int solution(int number) {
+		int countClap = 0;
+		for(int eachNumber = 1; eachNumber <= number; eachNumber++) {
+			if(String.valueOf(eachNumber).contains("3") || String.valueOf(eachNumber).contains("6") ||
+					String.valueOf(eachNumber).contains("9")) {
+				countClap = count369(String.valueOf(eachNumber), countClap);
+			}
+		}
+		return countClap;
+	}
 }
