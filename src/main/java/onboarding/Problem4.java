eb58f1e5bb47a9cc3a835d29f7ad8529f3b1a470
jthugg <jthug@naver.com>
2022-12-13 02:50:47 +0900

    Feat: 알파벳인지 체크하고 알파벳이면 변환하는 시퀀스 추가

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,23 @@
 package onboarding;
 
 public class Problem4 {
-    public static String solution(String word) {
-        String answer = "";
-        return answer;
-    }
+
+	public static String solution(String word) {
+		char[] spellsOfWord = word.toCharArray();
+		StringBuilder convertedWord = new StringBuilder();
+		for(char spell : spellsOfWord) {
+			convertedWord.append(checkSpell(spell));
+		}
+		return convertedWord.toString();
+	}
+
+	private static char checkSpell(char spell) {
+		if(isLowerCase(spell)) {
+			return convertLowerCase(spell);
+		}
+		if(isUpperCase(spell)) {
+			return convertUpperCase(spell);
+		}
+		return spell;
+	}
 }
