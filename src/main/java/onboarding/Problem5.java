c50d0730060a4931fbd43ca90a4c35d24cce8956
jthugg <jthug@naver.com>
2022-12-13 07:04:59 +0900

    Feat: 각 액면가 화폐의 개수를 정하고 리스트에 담는 기능 추가, 기능 구현 완료

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,30 @@
 package onboarding;
 
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Problem5 {
-    public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
-        return answer;
-    }
+
+	public static List<Integer> solution(int money) {
+		return collectMoney(money);
+	}
+
+	private static List<Integer> collectMoney(int money) {
+		List<Integer> wallet = new ArrayList<>();
+		for(int factor = 50000; factor >= 1; factor /= makeFactor(factor)) {
+			wallet.add(money / factor);
+			money %= factor;
+		}
+		return wallet;
+	}
+
+	private static int makeFactor(int factor) {
+		if(factor == 10) {
+			return 10;
+		}
+		if(String.valueOf(factor).charAt(0) == '5') {
+			return 5;
+		}
+		return 2;
+	}
 }
